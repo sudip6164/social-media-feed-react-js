271ec05461f528527eccf6a31d512ff42aa86c3c
sudip6164 <sudippradhanadgj@gmail.com>
2025-03-17 08:56:01 +0545

    Set Up Django REST API

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import Post, Comment, Like, User
+from .serializers import PostSerializer, CommentSerializer, LikeSerializer, UserSerializer
 
-# Create your views here.
+# Authentication
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class PostListCreate(generics.ListCreateAPIView):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class CommentListCreate(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class LikeListCreate(generics.ListCreateAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
